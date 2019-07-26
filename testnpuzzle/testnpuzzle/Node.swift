5fdf433eec03eea09dc2c2515d65a2ce16fe0f93
eric <eric@imac.local>
2019-07-26 02:44:00 +0200

    debut eucli

--- testnpuzzle/testnpuzzle/Node.swift
+++ testnpuzzle/testnpuzzle/Node.swift
@@ -12,25 +12,27 @@ class Node: Comparable, Hashable {
     let state: [[Int]]
     let parent: Node?
     let cost: Int
-    let heuristic: Int
+    var heuristic: Int
     let zeroRow: Int
     let zeroCol: Int
     let hash: String
+    let flatState: [Int]
     
     var score: Int {
         return cost + heuristic
     }
     
-    init(state: [[Int]], parent: Node?, zeroRow: Int, zeroCol: Int, cost: Int, heuristic: Int) {
+    init(state: [[Int]], parent: Node?, zeroRow: Int, zeroCol: Int, cost: Int, heuristic: Int = 0) {
         self.state = state
         self.parent = parent
         self.cost = cost
         self.heuristic = heuristic
         self.zeroRow = zeroRow
         self.zeroCol = zeroCol
+        self.flatState = Array(state.joined())
         
         var h = "";
-        for i in Array(state.joined()) {
+        for i in self.flatState {
             h += String(i)
         }
         self.hash = h;
@@ -68,4 +70,80 @@ class Node: Comparable, Hashable {
             nb = nb + 1
         }
     }
+    
+    
+    /*
+     OLD IMPLEMENTATION
+     */
+    
+    private func findDirections(from nilPosition: (Int, Int)?) -> [Direction] {
+        var directions: [Direction] = []
+        
+        if let pos = nilPosition {
+            if pos.0 > 0 {
+                directions.append(.up)
+            }
+            if pos.0 < self.state.count - 1 {
+                directions.append(.down)
+            }
+            if pos.1 > 0 {
+                directions.append(.left)
+            }
+            if pos.1 < self.state[0].count - 1 {
+                directions.append(.right)
+            }
+        }
+        return directions
+    }
+    
+    
+    private func findNilPosition() -> (Int, Int)? {
+        for (i, row) in self.state.enumerated() {
+            for (j, column) in row.enumerated() {
+                if column == 0 {
+                    return (i, j)
+                }
+            }
+        }
+        return nil
+    }
+    
+    private func myswap(from position: (Int, Int), to direction: Direction) -> [[Int]] {
+        var newState: [[Int]] = self.state
+        switch direction {
+        case .down:
+            newState[position.0][position.1] = newState[position.0 + 1][position.1]
+            newState[position.0 + 1][position.1] = 0
+        case .left:
+            newState[position.0][position.1] = newState[position.0][position.1 - 1]
+            newState[position.0][position.1 - 1] = 0
+        case .right:
+            newState[position.0][position.1] = newState[position.0][position.1 + 1]
+            newState[position.0][position.1 + 1] = 0
+        case .up:
+            newState[position.0][position.1] = newState[position.0 - 1][position.1]
+            newState[position.0 - 1][position.1] = 0
+        }
+        return newState;
+    }
+    
+    func findPossibleState() -> [[[Int]]] {
+        var possibleStates: [[[Int]]] = [];
+        let nilPosition = findNilPosition()
+        let directions = findDirections(from: nilPosition)
+        
+        if let pos = nilPosition {
+            for direction in directions {
+                let newState = myswap(from: pos, to: direction)
+                possibleStates.append(newState)
+            }
+        }
+        return possibleStates;
+    }
+}
+
+
+
+enum Direction {
+    case up, down, right, left
 }
--- testnpuzzle/testnpuzzle/main.swift
+++ testnpuzzle/testnpuzzle/main.swift
@@ -10,32 +10,34 @@ import Foundation
 
 
 
-var startState = [
+/*var startState = [
     [3, 13, 4, 5],
     [14, 8, 0, 6],
     [2, 15, 7, 9],
     [12, 10, 11, 1]
+]*/
+var startState = [
+    [0, 2, 3],
+    [1, 4, 5],
+    [8, 7, 6]
 ]
-//var startState = [
-//    [0, 2, 3],
-//    [1, 4, 5],
-//    [8, 7, 6]
-//]
-//
-//var goalState = [
-//    [1, 2, 3],
-//    [8, 0, 4],
-//    [7, 6, 5]
-//]
 
 var goalState = [
+    [1, 2, 3],
+    [8, 0, 4],
+    [7, 6, 5]
+]
+
+/*var goalState = [
     [1, 2, 3, 4],
     [12, 13, 14, 5],
     [11, 0, 15, 6],
     [10, 9, 8, 7]
 ]
+*/
 
 let SIZE = startState.count - 1;
+let WEIGHT = 1
 
 func findCoordinates(_ number: Int, in state: [[Int]]) -> (row: Int, col: Int)? {
     for i in 0...SIZE {
@@ -56,13 +58,33 @@ func manhattan(_ state: [[Int]]) -> Int {
                 continue
             }
             let (row, col) = findCoordinates(state[i][j], in: goalState)!
+            
             let absRow = abs(i - row)
             let absCol = abs(j - col)
+            
             distance += (absRow + absCol)
-            // print("For : ", state[i][j], "coord in goal : ", row, col, " absR: ", absRow, "absC : ", absCol, " distance: ", distance)
+            // print("New coord in goal:: row-> ", row, "  col-> ", col, "value: ", state[i][j], "absRow  \(absRow)  absCol \(absCol)   moveToGoal: \(absRow + absCol)  total: \(distance)")
         }
     }
     return distance;
+}
+
+func euclidean(_ state: [[Int]]) -> Int {
+    // p = (p1, p2) and q = (q1, q2)
+    var distance = 0.0
+    for i in 0...SIZE {
+        for j in 0...SIZE {
+            if (state[i][j] == 0) {
+                continue
+            }
+            let (row, col) = findCoordinates(state[i][j], in: goalState)!
+            
+            distance += sqrt(pow((Double(row - i)), 2) + pow((Double(col - j)), 2))
+            print("New coord in goal:: row-> ", row, "  col-> ", col, "value: ", state[i][j], "absRow  \(pow((Double(row - i)), 2))  absCol \(pow((Double(col - j)), 2))  total: \(distance)")
+        }
+    }
+    print("SQRT: ", distance)
+    return Int(sqrt(distance));
 }
 
 func childFactory(state: inout [[Int]], parent node: Node, _ x: Int, _ y: Int, _ newX: Int, _ newY: Int) -> Node{
@@ -71,7 +93,7 @@ func childFactory(state: inout [[Int]], parent node: Node, _ x: Int, _ y: Int, _
     state[newX][newY] = 0
     
     // create child
-    let child = Node(state: state, parent: node, zeroRow: newX, zeroCol: newY, cost: node.cost + 1, heuristic: manhattan(state))
+    let child = Node(state: state, parent: node, zeroRow: newX, zeroCol: newY, cost: node.cost + 1, heuristic: euclidean(state) * WEIGHT)
     return child
 }
 
@@ -122,9 +144,11 @@ func getChildren(_ node: Node) -> [Node] {
 let (x, y) = findCoordinates(0, in: startState)!
 let (goalX, goalY) = findCoordinates(0, in: goalState)!
 
-var root = Node(state: startState, parent: nil, zeroRow: x, zeroCol: y, cost: 0, heuristic: manhattan(startState))
+var root = Node(state: startState, parent: nil, zeroRow: x, zeroCol: y, cost: 0, heuristic: euclidean(startState) * WEIGHT)
 var goalNode = Node(state: goalState, parent: nil, zeroRow: goalX, zeroCol: goalY, cost: 0, heuristic: 0)
 
+//print(root.flatState)
+//print(goalNode.flatState)
 
 var count = 0;
 
@@ -132,10 +156,11 @@ func execute() {
     var openList = PriorityQueue(queue: [root])
     var closedList = Dictionary<String, Node>()
     
-    
     while (!openList.isEmpty) {
         let currentNode = openList.pop()
         let children = getChildren(currentNode!)
+        
+        // let children = oldGetChildren(current: currentNode!)
         closedList[currentNode!.hash] = currentNode
         
         for child in children {
@@ -159,7 +184,46 @@ func execute() {
     }
 }
 
+
+
+// ###############################    OLD
+func oldGetChildren(current: Node) -> [Node] {
+    var children: [Node] = [];
+    
+    let states = current.findPossibleState()
+    
+    for (childIndex, _) in states.enumerated() {
+        children.append(Node(state: states[childIndex], parent: current, zeroRow: -1, zeroCol: -1, cost: current.cost + 1, heuristic: manhattan(states[childIndex])))
+    }
+    return children
+}
+
+
+// ##########################  OLD
+func oldManhattanHeuristic(node: Node) -> Int{
+    var manhattanDistances = 0
+    for y in 0...SIZE {
+        for x in 0...SIZE {
+            if node.state[y][x] != 0 && node.state[y][x] != goalNode.state[y][x] {
+                let currentIndex = node.flatState.index(where: {$0 == node.state[y][x]})
+                let goalIndex = goalNode.flatState.index(where: {$0 == node.state[y][x]})
+                
+                let h = abs(currentIndex! / SIZE - goalIndex! / SIZE)
+                let w = abs(currentIndex! % SIZE - goalIndex! % SIZE)
+                
+                
+                manhattanDistances = manhattanDistances + h + w
+                print("Old coord in goal:: currentIndex-> ", currentIndex!, "  goalIndex-> ", goalIndex!, "value: ", node.state[y][x], "absRow  \(h)  absCol \(w)   moveToGoal: \(h + w)  total: \(manhattanDistances)")
+            }
+        }
+    }
+    return manhattanDistances
+}
+
+
 let startTime = CFAbsoluteTimeGetCurrent()
+print("started")
+
 execute()
 let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
 print("Time elapsed for : \(timeElapsed) s.")
